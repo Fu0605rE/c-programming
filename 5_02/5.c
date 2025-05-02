68a06262a56a5630269a8c256f908d9948906ee4
Fu0605rE <gsr-620-bgb@e-net.nara.jp>
2025-05-02 06:18:38 +0000

    5月2日

---
+++ 5_02/5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+int score;
+scanf("点数を入力してください:");
+scanf("%d",&score);
+if (score >88){
+     printf("合格です。\n");
+}else{
+    printf("不合格です。\n");
+return 0;
+}
+}
---
+++ 5_02/8.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+    int i;
+    for(int i=1; i<=10; i++){
+printf("1*%d=%d\n",i);
+    }
+    return 0;
+}
